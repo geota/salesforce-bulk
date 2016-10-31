6bf1b5b8ecb1a362edce77ebbd711ebe0a5837ab
Adrian Maceiras <amac425@gmail.com>
2016-10-31 15:24:47 -0500

    Bump versions

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE') as f:
 
 setup(
     name='salesforce-bulk',
-    version='1.1.0',
+    version='1.1.0-RMN',
     description='Python interface to the Salesforce.com Bulk API.',
     long_description=readme,
     author='Scott Persinger',
